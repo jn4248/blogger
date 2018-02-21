e43796437dffbb249d776f77638167b0ebe8ee73
Jason <jnelson2@gmail.com>
2018-02-21 16:49:34 -0500

    Add tagging model. Deletion of Tags.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,8 +36,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-  flash.notice = "Article Deleted: '#{@article.title}'"
-
+    flash.notice = "Article Deleted: '#{@article.title}'"
     redirect_to articles_path
   end
 

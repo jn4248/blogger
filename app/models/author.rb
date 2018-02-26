6f7bdf36e50db0d1f5bb0f524f7ba8dc9ec89cfc
Jason <jnelson2@gmail.com>
2018-02-26 17:17:15 -0500

    Add deletion of comments by loggedin user only

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ApplicationRecord
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: "should match confirmation", if: :password
+  validates_uniqueness_of :email, :username
 end
